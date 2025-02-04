9e763ef528f61b3910e6237af2ca92a8e5c4c269
JulianoAureliano <juliano.aureliano2013@gmail.com>
2025-02-04 17:38:16 -0300

    IMC pronto, indo para os retoques

--- imc/imcscript.js
+++ imc/imcscript.js
@@ -12,8 +12,30 @@ botaoCalcular.addEventListener("click", () => {
   ) {
     resultado.textContent = "Preencha todos os campos";
   } else {
-    let imc =
-      pesoDoUsuario.value / (alturaDoUsuario.value * alturaDoUsuario.value);
-    resultado.textContent = nomeDoUsuario.value + " Seu imc é : " + imc;
+    let imc = (
+      pesoDoUsuario.value /
+      (alturaDoUsuario.value * alturaDoUsuario.value)
+    ).toFixed(2);
+    console.log(imc);
+
+    if (imc >= 40) {
+      resultado.textContent =
+        nomeDoUsuario.value + " Seu imc é : " + imc + ". Obesidade grau 3!!";
+    } else if (imc >= 35 && imc < 39.9) {
+      resultado.textContent =
+        nomeDoUsuario.value + " Seu imc é : " + imc + ". Obesidade grau 2!!";
+    } else if (imc >= 30 && imc < 34.9) {
+      resultado.textContent =
+        nomeDoUsuario.value + " Seu imc é : " + imc + ". Obesidade grau 1!!";
+    } else if (imc >= 25 && imc < 29.9) {
+      resultado.textContent =
+        nomeDoUsuario.value + " Seu imc é : " + imc + ". Sobrepeso!!";
+    } else if (imc >= 18.6 && imc < 24.9) {
+      resultado.textContent =
+        nomeDoUsuario.value + " Seu imc é : " + imc + ". Normal!!";
+    } else if (imc <= 18.5) {
+      resultado.textContent =
+        nomeDoUsuario.value + " Seu imc é : " + imc + ". Abaixo do normal!!";
+    }
   }
 });
